f73b6e5c9065a50b293def5ef62063ffba8c1f4a
Ryan <ryan.mousa14@gmail.com>
2018-12-07 09:00:45 -0600

    Initlize the Project

---
+++ AltimetrikMusic/src/main/java/com/altimetrik/music/AltimetrikMusic/AltimetrikMusicApplication.java
@@ -0,0 +1,12 @@
+package com.altimetrik.music.AltimetrikMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltimetrikMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltimetrikMusicApplication.class, args);
+	}
+}
